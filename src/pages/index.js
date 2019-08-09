5af81fa22aa5c28325eeb5c1b51b3b870a3e2624
marinov98 <wbbmarin5@gmail.com>
2019-08-09 16:41:04 -0400

    changed back name font

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const IndexPage = () => (
     <div id="index">
         <h1>Hello and Welcome to my Website!</h1>
-        <h3>My name is 𝓜𝓪𝓻𝓲𝓷 𝓟. 𝓜𝓪𝓻𝓲𝓷𝓸𝓿</h3>
+        <h3>My name is 🅼🅰🆁🅸🅽 🅿. 🅼🅰🆁🅸🅽🅾🆅</h3>
     </div>
 );
 

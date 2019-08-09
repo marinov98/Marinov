893e9d3e7039cfb0d0e23d77beef0a4fc2573577
marinov98 <wbbmarin5@gmail.com>
2019-08-09 15:03:30 -0400

    added new font and compnents

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const IndexPage = () => (
     <div id="index">
         <h1>Hello and Welcome to my Website!</h1>
-        <h3>
-            My name is <span role="img">🔊 𝓜𝓪𝓻𝓲𝓷 𝓟. 𝓜𝓪𝓻𝓲𝓷𝓸𝓿 🔊</span>
-        </h3>
+        <h3>My name is 𝓜𝓪𝓻𝓲𝓷 𝓟. 𝓜𝓪𝓻𝓲𝓷𝓸𝓿</h3>
     </div>
 );
 

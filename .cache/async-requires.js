63a267f92967aca777b0fe3a3749936bbeb413b8
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-08-08 23:29:01 -0400

    gatsby template for personal site generated

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/home/marin/Desktop/Marinov/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/home/marin/Desktop/Marinov/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/home/marin/Desktop/Marinov/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/home/marin/Desktop/Marinov/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+

3ece6a2995c91ab54941badc2f74674f78e41561
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-08-09 21:47:16 -0400

    navbar finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import NavBar from './../components/NavBar/NavBar';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const IndexPage = () => (
     <div id="index">
-        <h1>Hello and Welcome to my Website!</h1>
-        <h3>My name is 🅼🅰🆁🅸🅽 🅿. 🅼🅰🆁🅸🅽🅾🆅</h3>
+        <div id="nav">
+            <NavBar />
+        </div>
+        <h1 style={{ marginTop: '70px' }}>Hello and Welcome to my Website!</h1>
+        <h3>My name is 🅜🅐🅡🅘🅝 🅟. 🅜🅐🅡🅘🅝🅞🅥</h3>
     </div>
 );
 

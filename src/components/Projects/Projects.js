a04313de7c3d31b1a435f36ee3e47caeb304d2e6
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-02-01 17:06:22 -0500

    completed adding new projects and demo buttons

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,47 +1,11 @@
 import React from 'react';
 import './Projects.css';
 import ScrollAnimation from 'react-animate-on-scroll';
+import { Button } from 'reactstrap';
 
 export default function Projects() {
     return (
         <div id="projects">
-            <ScrollAnimation animateIn="bounceInDown" animateOnce="true">
-                <div className="row all-projects">
-                    <div className="calculator-project col project">
-                        <h4>Statistical Calculator</h4>
-                        <a
-                            href="https://github.com/marinov98/Statistical_Calculator"
-                            target="_blank"
-                        >
-                            <img
-                                src={require('../../images/projects/stat-logo.jpeg')}
-                                alt="stat calculator"
-                            ></img>
-                        </a>
-                        <h5 style={{ marginTop: '7px' }}>Tech Stack:</h5>
-                        <p>C++, GNU Make/Cmake</p>
-                        <h6>Description:</h6>
-                        <p>
-                            Simple Terminal-based calculator that can calculate 1 & 2 variable
-                            statistics confidence intervals,conduct hypothesis testing, and find
-                            correlation coefficients.
-                        </p>
-                    </div>
-                    <div className="instaPet-project col project">
-                        <h4>InstaPet</h4>
-                        <a href="https://github.com/angelalim1010/InstaPet" target="_blank">
-                            <img
-                                src={require('../../images/projects/instapet.png')}
-                                alt="InstaPet"
-                            ></img>
-                        </a>
-                        <h5 style={{ marginTop: '7px' }}>Tech Stack:</h5>
-                        <p>React.js, Redux.js, Node.js, Express.js, PostreSQL, Json Web Tokens</p>
-                        <h6>Description:</h6>
-                        <p>Group project aimed to build an app like Instagram... but for pets.</p>
-                    </div>
-                </div>
-            </ScrollAnimation>
             <ScrollAnimation animateIn="bounceInDown" animateOnce="true">
                 <div className="row all-projects">
                     <div className="col clusterDuck project">
@@ -65,6 +29,63 @@ export default function Projects() {
                             questions/advice about computer science and tag their post with a
                             specific class if they so wish. Students can reply to one another, like
                             each others post and view each other's profiles
+                        </p>
+                        <div
+                            style={{
+                                textAlign: 'center',
+                                marginBottom: '3px'
+                            }}
+                        >
+                            <a href="https://cluster-duck.herokuapp.com/login" target="_blank">
+                                <Button color="secondary">Demo</Button>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="instaPet-project col project">
+                        <h4>InstaPet</h4>
+                        <a href="https://github.com/angelalim1010/InstaPet" target="_blank">
+                            <img
+                                src={require('../../images/projects/instapet.png')}
+                                alt="InstaPet"
+                            ></img>
+                        </a>
+                        <h5 style={{ marginTop: '7px' }}>Tech Stack:</h5>
+                        <p>React.js, Redux.js, Node.js, Express.js, PostreSQL, Json Web Tokens</p>
+                        <h6>Description:</h6>
+                        <p>Group project aimed to build an app like Instagram... but for pets.</p>
+                        <div
+                            style={{
+                                textAlign: 'center',
+                                marginBottom: '3px'
+                            }}
+                        >
+                            <a href="https://ttp-instapet.herokuapp.com/" target="_blank">
+                                <Button color="secondary">Demo</Button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="bounceInDown" animateOnce="true">
+                <div className="row all-projects">
+                    <div className="calculator-project col project">
+                        <h4>Statistical Calculator</h4>
+                        <a
+                            href="https://github.com/marinov98/Statistical_Calculator"
+                            target="_blank"
+                        >
+                            <img
+                                src={require('../../images/projects/stat-logo.jpeg')}
+                                alt="stat calculator"
+                            ></img>
+                        </a>
+                        <h5 style={{ marginTop: '7px' }}>Tech Stack:</h5>
+                        <p>C++, GNU Make/Cmake</p>
+                        <h6>Description:</h6>
+                        <p>
+                            Simple Terminal-based calculator that can calculate 1 & 2 variable
+                            statistics confidence intervals,conduct hypothesis testing, and find
+                            correlation coefficients.
                         </p>
                     </div>
                     <div className="col dotfiles project">
@@ -111,6 +132,19 @@ export default function Projects() {
                             Aims to deliver healthy food fast. Contains its own plans and sign up
                             forms.
                         </p>
+                        <div
+                            style={{
+                                textAlign: 'center',
+                                marginBottom: '3px'
+                            }}
+                        >
+                            <a
+                                href="https://divine-and-healthy-foods.herokuapp.com/"
+                                target="_blank"
+                            >
+                                <Button color="secondary">Demo</Button>
+                            </a>
+                        </div>
                     </div>
                     <div className="col weather-guardian project">
                         <h4>Weather Guardian</h4>
@@ -121,7 +155,7 @@ export default function Projects() {
                             ></img>
                         </a>
                         <h5 style={{ marginTop: '7px' }}>Tech Stack:</h5>
-                        <p>React.js, Node.js, Express.js, IBM weather API</p>
+                        <p>Yarn, React.js, Node.js, Express.js, IBM weather API</p>
                         <h6>Description:</h6>
                         <p>
                             App created to warn users if there has been a fire nearby and the wind
@@ -129,6 +163,16 @@ export default function Projects() {
                             user or their family has asthma or live in an area that is susceptible
                             to fire.{' '}
                         </p>
+                        <div
+                            style={{
+                                textAlign: 'center',
+                                marginBottom: '3px'
+                            }}
+                        >
+                            <a href="https://weather-guardian.herokuapp.com/" target="_blank">
+                                <Button color="secondary">Demo</Button>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </ScrollAnimation>
